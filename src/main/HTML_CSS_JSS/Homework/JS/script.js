170381a6ba5278cefa9db4ee519c6ad2c4e01e9f
mcsbukos <mcsbukos@gmail.com>
2023-06-09 23:20:02 +0300

    InitialCommit

---
+++ src/main/HTML_CSS_JSS/Homework/JS/script.js
@@ -0,0 +1,27 @@
+function clickMe() {
+     readInputText();
+}
+
+function readInputText() {
+    var usernameText = document.getElementById("username").value;
+    var passwordText = document.getElementById("pass").value;
+    var passrepeatText = document.getElementById("passrepeat").value;
+    var txtarea1Text = document.getElementById("txtarea1").value;
+    var emailText = document.getElementById("email").value;
+
+   if (usernameText.length === 0) {
+             alert("Please enter the username");
+           }
+    if (passwordText.length === 0) {
+            alert("Please Enter the password");
+        }
+    if (passrepeatText.length === 0) {
+             alert("Please repeat the password");
+         }
+    if (txtarea1Text.length === 0) {
+                 alert("Please enter the Address");
+             }
+    if (emailText.length === 0) {
+                     alert("Please enter the Email address");
+                 }
+}
