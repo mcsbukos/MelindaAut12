57a98ad16be855068dc020ce1fd7c005d7016ef0
mcsbukos <mcsbukos@gmail.com>
2023-06-17 13:03:30 +0300

    InitialCommit

---
+++ src/main/java/ro/sit/ro/course/Course03/Rectangle.java
@@ -0,0 +1,14 @@
+package ro.sit.ro.course.Course03;
+
+public class Rectangle {
+    double width;
+    double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public static class Methods {
+    }
+}
--- src/main/java/ro/sit/ro/course/Course03/Shapes.java
+++ src/main/java/ro/sit/ro/course/Course03/Shapes.java
@@ -2,16 +2,20 @@ package ro.sit.ro.course.Course03;
 
 public class Shapes {
     public static void main(String[] args) {
-        Square square = new Square();
-        square.setSquareSide(Double.parseDouble(args[0]));
-        System.out.println(square.getArea());
-        System.out.println();
+       // Square square = new Square();
+      //  square.setSquareSide(Double.parseDouble(args[0]));
+       // System.out.println(square.getArea());
+       // System.out.println();
 
-        for(String argument : args) {
-            Square currentSquare = new Square();
-            currentSquare.setSquareSide(Double.parseDouble(argument));
-            System.out.println(square.getArea());
-        }
+       // for(String argument : args) {
+       //     Square currentSquare = new Square();
+        //    currentSquare.setSquareSide(Double.parseDouble(argument));
+         //   System.out.println(square.getArea());
+    Rectangle rectangle = new Rectangle(Double.parseDouble(args[0])){
+            Double.parseDouble(args[1]);
+        System.out.println(rectangle.width);
+        System.out.println(rectangle.length);
+    }
 
     }
 
